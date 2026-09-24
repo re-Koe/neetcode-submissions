5bd94bbc38faeca3d4907acc82454863a074eb9f
re-Koe <105235665+re-Koe@users.noreply.github.com>
2026-09-23 22:21:09 -0600

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        aset = set()
+
+        for i in nums:
+            if i in aset:
+                return True
+            aset.add(i)
+        return False
